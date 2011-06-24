23074de5deb6e9aa78718e98167113ec623b9961
Anand Patil <anand.prabhakar.patil@gmail.com>
2011-06-24 09:59:17 +0100

    Added delay

--- ibdw/__init__.py
+++ ibdw/__init__.py
@@ -127,7 +127,7 @@ areal_postproc = [area_allele, area_hw_homo, area_hw_hetero, area_hw_any]
 def mcmc_init(M):
     scalar_vars = [M.amp, M.amp_short_frac, M.scale_short, M.scale_long, M.diff_degree, M.m, M.V]
     scales = dict([(k,.0001) for k in scalar_vars])
-    M.use_step_method(pm.gp.GPParentAdaptiveMetropolis, scalar_vars, scales=scales)
+    M.use_step_method(pm.gp.GPParentAdaptiveMetropolis, scalar_vars, scales=scales, delay=20000)
     M.use_step_method(pm.gp.GPEvaluationGibbs, M.sp_sub, M.V, M.eps_p_f_d)
                     
 metadata_keys = ['fi','ti','ui']
